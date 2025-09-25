130856126e26141c9189a1f6065f74d3fea5f703
romaiy <sokolovgit256@gmail.com>
2025-09-25 19:20:57 +0600

    feat(ui): добавил иконки

--- packages/frontend/lib/ui/components/Icon/index.ts
+++ packages/frontend/lib/ui/components/Icon/index.ts
@@ -4,6 +4,8 @@ import ExportIcon from './_icons/export.svg?react';
 import GeneralIcon from './_icons/general.svg?react';
 import MessageIconM from './_icons/message-m.svg?react';
 import MessageIconS from './_icons/message-s.svg?react';
+import MoodSadIcon from './_icons/mood-sad.svg?react';
+import MoodSmileIcon from './_icons/mood-smile.svg?react';
 import SearchIcon from './_icons/search.svg?react';
 import SelectorIcon from './_icons/selector.svg?react';
 import StatIcon from './_icons/stat.svg?react';
@@ -16,6 +18,8 @@ export {
   GeneralIcon,
   MessageIconM,
   MessageIconS,
+  MoodSadIcon,
+  MoodSmileIcon,
   SearchIcon,
   SelectorIcon,
   StatIcon,
