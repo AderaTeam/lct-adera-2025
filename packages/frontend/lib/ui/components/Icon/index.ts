8b85aac8ade6f7de8b414385d77f21b859505a79
romaiy <sokolovgit256@gmail.com>
2025-09-25 16:29:31 +0600

    fix(ui): ввел сайзинг иконок

--- packages/frontend/lib/ui/components/Icon/index.ts
+++ packages/frontend/lib/ui/components/Icon/index.ts
@@ -1,10 +1,23 @@
-import ArrowsSortIcon from './_icons/arrows-sort.svg?react';
+import ArrowsSortIconM from './_icons/arrows-sort-m.svg?react';
+import ArrowsSortIconS from './_icons/arrows-sort-s.svg?react';
 import ExportIcon from './_icons/export.svg?react';
 import GeneralIcon from './_icons/general.svg?react';
-import MessageIcon from './_icons/message.svg?react';
+import MessageIconM from './_icons/message-m.svg?react';
+import MessageIconS from './_icons/message-s.svg?react';
 import SearchIcon from './_icons/search.svg?react';
 import SelectorIcon from './_icons/selector.svg?react';
 import StatIcon from './_icons/stat.svg?react';
 import XIcon from './_icons/x.svg?react';
 
-export { ArrowsSortIcon, ExportIcon, GeneralIcon, MessageIcon, SearchIcon, SelectorIcon, StatIcon, XIcon };
+export {
+  ArrowsSortIconM,
+  ArrowsSortIconS,
+  ExportIcon,
+  GeneralIcon,
+  MessageIconM,
+  MessageIconS,
+  SearchIcon,
+  SelectorIcon,
+  StatIcon,
+  XIcon
+};
