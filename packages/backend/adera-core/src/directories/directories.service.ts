b1e15b8d8e877bb76a6e2b163a031131699d48df
romaiy <sokolovgit256@gmail.com>
2025-09-27 18:15:30 +0600

    feat(c): добавил моки продуктов

--- packages/backend/adera-core/src/directories/directories.service.ts
+++ packages/backend/adera-core/src/directories/directories.service.ts
@@ -13,6 +13,20 @@ export class DirectoriesService {
   }
 
   async findProducts(): Promise<ApiDirectory[]> {
-    return [];
+    return [
+      { name: 'Счет', id: 1 },
+      { name: 'Мобильное приложение', id: 2 },
+      { name: 'Ипотека', id: 3 },
+      { name: 'Дебетовые карты', id: 4 },
+      { name: 'online сервис', id: 5 },
+      { name: 'Банковские сейфы', id: 6 },
+      { name: 'Денежные переводы', id: 7 },
+      { name: 'Депозитарные услуги', id: 8 },
+      { name: 'Мобильное приложение2', id: 9 },
+      { name: 'Инвестиции и брокерское обслуживание', id: 10 },
+      { name: 'Обмен валют', id: 11 },
+      { name: 'Погашение кредита', id: 12 },
+      { name: 'Кредитный карты', id: 13 },
+    ];
   }
 }
