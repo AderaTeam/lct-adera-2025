eafd2161287fb2bd28bed21fe76ab723df0238be
romaiy <sokolovgit256@gmail.com>
2025-09-27 14:08:45 +0600

    feat(ui): добавил новые иконки

--- packages/frontend/lib/ui/components/Icon/index.ts
+++ packages/frontend/lib/ui/components/Icon/index.ts
@@ -1,11 +1,13 @@
 import ArrowsSortIconM from './_icons/arrows-sort-m.svg?react';
 import ArrowsSortIconS from './_icons/arrows-sort-s.svg?react';
+import ChevronDownIcon from './_icons/chevron-down.svg?react';
 import ExportIcon from './_icons/export.svg?react';
 import GeneralIcon from './_icons/general.svg?react';
 import MessageIconM from './_icons/message-m.svg?react';
 import MessageIconS from './_icons/message-s.svg?react';
 import MoodSadIcon from './_icons/mood-sad.svg?react';
 import MoodSmileIcon from './_icons/mood-smile.svg?react';
+import RefreshIcon from './_icons/refresh.svg?react';
 import SearchIcon from './_icons/search.svg?react';
 import SelectorIcon from './_icons/selector.svg?react';
 import StatIcon from './_icons/stat.svg?react';
@@ -14,12 +16,14 @@ import XIcon from './_icons/x.svg?react';
 export {
   ArrowsSortIconM,
   ArrowsSortIconS,
+  ChevronDownIcon,
   ExportIcon,
   GeneralIcon,
   MessageIconM,
   MessageIconS,
   MoodSadIcon,
   MoodSmileIcon,
+  RefreshIcon,
   SearchIcon,
   SelectorIcon,
   StatIcon,
