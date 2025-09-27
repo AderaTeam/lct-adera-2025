e0b3f12ab85c9f95d152882d0293bf68fdf20634
romaiy <sokolovgit256@gmail.com>
2025-09-27 21:55:35 +0600

    feat(c): добавил s3 модуль

--- packages/backend/adera-core/src/app/app.module.ts
+++ packages/backend/adera-core/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DirectoriesModule } from 'src/directories/directories.module';
+import { S3Module } from 'src/s3/s3.module';
 
 @Module({
-  imports: [DirectoriesModule],
+  imports: [DirectoriesModule, S3Module],
   controllers: [AppController],
   providers: [AppService],
 })
