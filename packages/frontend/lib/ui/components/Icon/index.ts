c7f61310829712baccffaa0360b008005603601d
romaiy <sokolovgit256@gmail.com>
2025-09-27 19:36:57 +0600

    feat(ui): добавил новые иконки

--- packages/frontend/lib/ui/components/Icon/index.ts
+++ packages/frontend/lib/ui/components/Icon/index.ts
@@ -1,8 +1,12 @@
+import ArrowsRightIcon from './_icons/arrow-right.svg?react';
 import ArrowsSortIconM from './_icons/arrows-sort-m.svg?react';
 import ArrowsSortIconS from './_icons/arrows-sort-s.svg?react';
 import ChevronDownIcon from './_icons/chevron-down.svg?react';
+import CircleCheckFilledIcon from './_icons/circle-check-filled.svg?react';
+import DownloadIcon from './_icons/download.svg?react';
 import ExportIcon from './_icons/export.svg?react';
 import GeneralIcon from './_icons/general.svg?react';
+import LoaderIcon from './_icons/loader.svg?react';
 import MessageIconM from './_icons/message-m.svg?react';
 import MessageIconS from './_icons/message-s.svg?react';
 import MoodSadIcon from './_icons/mood-sad.svg?react';
@@ -11,14 +15,19 @@ import RefreshIcon from './_icons/refresh.svg?react';
 import SearchIcon from './_icons/search.svg?react';
 import SelectorIcon from './_icons/selector.svg?react';
 import StatIcon from './_icons/stat.svg?react';
+import UploadIcon from './_icons/upload.svg?react';
 import XIcon from './_icons/x.svg?react';
 
 export {
+  ArrowsRightIcon,
   ArrowsSortIconM,
   ArrowsSortIconS,
   ChevronDownIcon,
+  CircleCheckFilledIcon,
+  DownloadIcon,
   ExportIcon,
   GeneralIcon,
+  LoaderIcon,
   MessageIconM,
   MessageIconS,
   MoodSadIcon,
@@ -27,5 +36,6 @@ export {
   SearchIcon,
   SelectorIcon,
   StatIcon,
+  UploadIcon,
   XIcon
 };
