467ce6ee08bd3b097b8fc9a33978b099ee4852c0
self <self@self.ru>
2025-10-02 21:37:59 +0600

    feat(c): обновил постман и удалил ненужные эндпоинты

--- packages/backend/adera-core/src/app/app.controller.ts
+++ packages/backend/adera-core/src/app/app.controller.ts
@@ -12,22 +12,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Get("amount-dynamic")
-  getAmountDynamic(): AmountDynamicDto[]{
-    return [{name: "a", amount: 10000}]
-    //TODO
-  }
-
-  @Get("tone-dynamic")
-  getToneDynamic(): ToneDynamicDto[]{
-    return [{name: "Январь", negative: 10, neutral: 240, positive: 500}]
-    //TODO
-  }
-
-  @Get("main-analytics")
-  getMainAnalytics(): MainAnalytiscDataDto{
-    return {counts: {negativeCount: 1, neutralCount: 2, positiveCount: 3}, topics: [{name: "Кредит", negativeCount: 1, neutralCount: 2, positiveCount: 4}]}
-    //TODO
-  }
 }
